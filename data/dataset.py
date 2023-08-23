2dcab2ba8bd6d2cde153be5f3bd4860bfd2fcd6a
mmmmimic <matthewlin98@hotmail.com>
2023-08-23 20:21:46 +0800

    [FEATURE] add dataset for dvm tabular data

--- data/dataset.py
+++ data/dataset.py
@@ -3,14 +3,17 @@ from torch.utils.data import Dataset
 import pandas as pd
 import numpy as np
 import json
+from PIL import Image
+from tabular_tools import OneHotEmbedder, DefaultEmbedder
 
 class DVM(Dataset):
-    def __init__(self, split: str, transforms: dict, numerical: bool=False) -> None:
+    def __init__(self, split: str, transforms: dict, numerical: bool=False, tab_embedder: Any=DefaultEmbedder) -> None:
         '''
         Args:
             split (str): data split, can be 'train', 'val', or 'test'
             transforms (dict): transforms on tabular data and image data, stored in a dictionary {'tab_tf': ..., 'img_tf': ...}
             numerical (bool): whether use normalized and categorized numerical values for each cell
+            tab_embedder (any): a generator encoding tabular values into embeddings 
         '''
         super(DVM).__init__()
         self.split = split
@@ -28,16 +31,27 @@ class DVM(Dataset):
             for c in list(self.meta_info.keys()):
                 self.df[c] = self.df[c+'_num'].values
         
-        self.df = self.df[list(self.meta_info.keys())]
+        self.tab_embedder = tab_embedder
+        
+        self.image_paths = np.load(f'data/dvm_car/{split}_paths.npy')
         
     def __len__(self):
         return len(self.df)
     
     def __getitem__(self, index: int) -> dict:
-        data = {} # wrap up everything in a dictionary
+        tab_tf, img_tf = self.transforms['tab_tf'], self.transforms['img_tf']
         # load tabular data
+        tab_line = self.tab_embedder.get_line(self.df, self.meta_info, index)
+        tab_line = tab_tf(tab_line)
+
+        # load image data
+        image = Image.open(self.image_paths[index])
+        image = img_tf(image)
         
-        return {}
+        return { # wrap up everything in a dictionary
+                'image': image,
+                'tab_line': tab_line
+                }
     
     def __repr__(self) -> str:
         return super().__repr__()
@@ -51,11 +65,18 @@ if __name__ == "__main__":
         # onehot embedding
         pass
     transforms = {
-        'tab_tf': None, 
+        'tab_tf': lambda x: x, 
         'img_tf': T.Compose(
             [
-                T.ToTensor(),
+                T.ToTensor()
             ]
         )
     }
-    trainset = DVM(split='train', transforms=transforms, numerical=True)
+    trainset = DVM(split='train', transforms=transforms, numerical=True, tab_embedder=OneHotEmbedder())
+    data = trainset[100]
+    image = data['image']
+    tab_line = data['tab_line']
+    print(tab_line, tab_line.shape)
+    plt.figure()
+    plt.imshow(image.permute(1,2,0).numpy())
+    plt.show()
